80f0dfcc33bb563a43ba9e265e43b4048f6f4d74
Jason Chung <jasonckc07@gmail.com>
2021-01-11 15:35:53 -0500

    Add client folder

---
+++ client/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	semi: false,
+	trailingComma: "all",
+	singleQuote: true,
+	printWidth: 120,
+	tabWidth: 2,
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	arrowParens: "always"
+};
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
-const port = process.env.PORT || 8000;
+const app = express()
+const port = process.env.PORT || 8000
 
-app.get('/', (req, res) => res.send('Trackspace API'));
+app.get('/', (req, res) => res.send('Trackspace API'))
 
 app.listen(port, () => {
-    console.log(`⚡️ Server is running at https://localhost:${port}`);
-});
+  console.log(`⚡️ Server is running at https://localhost:${port}`)
+})
