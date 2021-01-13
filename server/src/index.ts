451004c05be9a81d5f0d253cc316c6b5cc26b6da
Jason Chung <jasonckc07@gmail.com>
2021-01-13 17:28:32 -0500

    Update static files path

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,12 +4,10 @@ import path from 'path'
 const app = express()
 const port = process.env.PORT || 8000
 
-app.get('/', (req, res) => res.send('Trackspace API'))
-
 // Serve static files from the React app.
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
 app.listen(port, () => {
